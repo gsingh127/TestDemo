4d2d6f9a16fc579685524e1056b2c9b4cb5bba50
gsingh127 <qa.gopal.singh@gmail.com>
2023-01-12 18:13:31 +0530

    Created JSON Reader method to read JSONFile & 2 methods to validate presence of wicketKeeper and less than 4 foreign players

--- src/main/java/TestMethods/TestMethod.java
+++ src/main/java/TestMethods/TestMethod.java
@@ -1,5 +1,43 @@
 package TestMethods;
+import java.io.IOException;
 
-public class TestMethod {
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import Utility.JsonReader;
+
+public class TestMethod extends JsonReader {
+
+static JSONObject PlayerJsonObject = readJSONFile();
+	
+public static int numberofForeignPlayers() throws IOException {
+		
+		JSONArray players = (JSONArray) PlayerJsonObject.get("player");
+		int countOfFP = 0;
+		for(int i=0;i<players.size();i++) {
+			
+			JSONObject obj =(JSONObject) players.get(i);
+			if(!(obj.get("country").toString().equals("India"))) {
+				countOfFP++;
+			}
+		}
+		return countOfFP;
+	}
+
+
+public static int numberofWicketKeepers() throws IOException {
+		
+		JSONArray players = (JSONArray) PlayerJsonObject.get("player");
+		int countOfWK = 0;
+		for(int i=0;i<players.size();i++) {
+			
+			JSONObject obj =(JSONObject) players.get(i);
+			if(obj.get("role").toString().equals("Wicket-keeper")) {
+				countOfWK++;
+			}
+		}
+		return countOfWK;
+	}
+	
+
 
 }
--- src/main/java/Utility/JsonReader.java
+++ src/main/java/Utility/JsonReader.java
@@ -1,5 +1,31 @@
 package Utility;
 
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class JsonReader {
+	
+	 static String  path = "testfiles//TeamRCB.json";
+	 static JSONParser parser = new JSONParser();
+	 static JSONObject jsonObject = null ;
+	
+
+
+public static  JSONObject readJSONFile()  {
+
+		try {
+			 jsonObject = (JSONObject) parser.parse(new FileReader(path));
+			 
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		return jsonObject;
+	
+	
+		
+		}
 
 }
