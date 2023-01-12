755c4d737a4d091d98d970b5987587cfe19f3ab5
gsingh127 <qa.gopal.singh@gmail.com>
2023-01-12 18:39:56 +0530

    Created test methods to validate testcases

---
+++ src/test/java/TestCases/TeamSelectionTest.java
@@ -0,0 +1,29 @@
+package TestCases;
+import java.io.IOException;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+import TestMethods.TestMethod;
+
+
+public class TeamSelectionTest {
+
+	@Test
+	public void validateForeignPlayerLimit() throws IOException {
+		
+	
+	int foreignPlayers	= TestMethod.numberofForeignPlayers();
+	Assert.assertTrue(foreignPlayers<=4);
+
+
+	}
+
+	
+	@Test
+	public void validateWicketKeeperAvailability() throws IOException {
+		
+	
+		int wicketKeepers	= TestMethod.numberofWicketKeepers();
+		Assert.assertTrue(wicketKeepers>=1);
+	
+	}
+}
